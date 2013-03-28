eebf0fc99a20f5a5f1ded9737d60da7e8522848c
abhisdub <abhisdub@cs.duke.edu>
2013-03-28 11:57:03 -0400

    again

--- Test.java
+++ Test.java
@@ -1,28 +1,28 @@
 /* Name: Dubey, Abhishek Kumar
    Date: March 28, 2013
-*/
+ */
 
 package duke.db.genome-analysis
 
 class Test {
   int test_number = 0;
-  
+
   public Test() {
-	  // do nothing
+    // do nothing
   }
-  
+
   public Test(int num) {
-	  this.test_number = num;
+    this.test_number = num;
   }
-  
+
   public Test(int num, boolean b) {
-	  // do nothing
+    // do nothing
   }
-  
+
   public static void main(String[] args) {
-	  int a =1;
-	  a++;
-	  a--;
+    int a =1;
+    a++;
+    a--;
   }
 }
 
